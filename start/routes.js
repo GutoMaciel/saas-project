e2104df5f39c38fe39094e235eab2eb7544203e2
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-12 21:40:36 -0300

    Authorization routes

--- start/routes.js
+++ start/routes.js
@@ -20,6 +20,7 @@ Route.post('sessions', 'SessionController.store').validator('Session')
 Route.post('users', 'UserController.store').validator('User')
 
 Route.group(() => {
+  Route.get('roles', 'RoleController.index')
   Route.resource('teams', 'TeamController')
     .apiOnly()
     .validator(
@@ -35,18 +36,10 @@ Route.group(() => {
 }).middleware('auth')
 
 Route.group(() => {
-  Route.post('invites', 'InviteController.store').validator('Invite')
+  Route.post('invites', 'InviteController.store').validator('Invite').middleware('can:invites_create')
 
   Route.resource('projects', 'ProjectController')
     .apiOnly()
-    .validator(
-      new Map(
-        [
-          [
-            ['projects.store', 'projects.update'],
-            ['Project']
-          ]
-        ]
-      )
-    )
+    .validator(new Map([[['projects.store', 'projects.update'], ['Project']]]))
+    .middleware(new Map([[['projects.store', 'projects.update'], ['can:projects_create']]]))
 }).middleware(['auth', 'team'])
