5f4146e871fa1c3a9177e3e4db67c7dba621562c
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-11-12 22:00:09 -0300

    Permissions Route

--- start/routes.js
+++ start/routes.js
@@ -45,4 +45,5 @@ Route.group(() => {
 
   Route.get('members', 'MemberController.index')
   Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
